b18f910d9fbb96b39af3dbf32e5e6f716844242f
David Metzler <david.metzler@rwu.de>
2023-05-13 11:30:32 +0200

    Fixed SSID not showing up

--- app/src/main/java/com/example/bequiet/model/receivers/WifiListener.java
+++ app/src/main/java/com/example/bequiet/model/receivers/WifiListener.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.net.wifi.ScanResult;
+import android.net.wifi.SupplicantState;
 import android.net.wifi.WifiInfo;
 import android.net.wifi.WifiManager;
 import android.util.Log;
@@ -22,8 +23,15 @@ public class WifiListener extends BrodcastReceiver {
             boolean connected = intent.getBooleanExtra(WifiManager.EXTRA_SUPPLICANT_CONNECTED, false);
             if (connected) {
                 WifiManager wifiManager = (WifiManager) context.getSystemService(Context.WIFI_SERVICE);
-                WifiInfo wifiInfo = wifiManager.getConnectionInfo();
-                Log.d("WifiReceiver", wifiInfo.toString());
+                //Array needs to be final to access it inside runnable.
+                final WifiInfo[] wifiInfo = {wifiManager.getConnectionInfo()};
+                // Connecting takes a while so we wait until the INfo is there.
+                new Thread(() -> {
+                    while (wifiInfo[0].getSupplicantState() != SupplicantState.COMPLETED){
+                        wifiInfo[0] = wifiManager.getConnectionInfo();
+                    }
+                    Log.d("WifiReceiver", wifiInfo[0].toString());
+                }).start();
             }
         }
     }
